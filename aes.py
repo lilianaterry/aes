3ede644813fcaf4bdcf2a32519eefa48924215a2
Connor Lewis <cmlewis95@gmail.com>
2018-09-15 17:27:38 -0500

    Adds matrix multiplaction

--- aes.py
+++ aes.py
@@ -1,10 +1,16 @@
 
 class AES:
+    sub_matrix = [
+        [2, 3, 1, 1],
+        [1, 2, 3, 1],
+        [1, 1, 2, 3],
+        [3, 1, 1, 2]
+    ]
+
+
     def __init__(self, key, keysize):
         self.key = key
-        self.keysize = keysize
-        for byte in self.key:
-            print(byte)
+        self.keysize = keysize                 
 
 
     def encrypt_file(self, inputfile, outfile):
@@ -28,4 +34,29 @@ class AES:
 
 
     def __add_roundkey(self):
+        pass
+
+
+    @staticmethod
+    def __matrix_multiply(mat_a, mat_b):
+        out_m = len(mat_a)
+        out_n = len(mat_b[0])
+
+        out_mat = []
+        for _ in range(out_m):
+            row = []
+            for _ in range(out_n):
+                row.append(0)
+            out_mat.append(row)
+
+        for out_r in range(out_m):
+            for out_c in range(out_n):
+                for other_val in range(len(mat_a[0])):
+                    out_mat[out_r][out_c] ^= AES.__mult(mat_a[out_r][other_val], mat_b[other_val][out_c])
+
+        return out_mat
+
+
+    @staticmethod
+    def __mult(int_a, int_b):
         pass
